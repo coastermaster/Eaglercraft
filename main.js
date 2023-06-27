5a9e4d1621a525a3f42fb4a955ca62e5beea1a17
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-06-27 16:23:27 +0500

    Create main.js
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,6 @@
+import eaglercraftOpts from './config.js';
+
+// Access the configuration object
+console.log(eaglercraftOpts.container); // Output: "game_frame"
+console.log(eaglercraftOpts.relays); // Output: Array of relay objects
+// ... access other properties of eaglercraftOpts as needed
